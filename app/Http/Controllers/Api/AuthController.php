75edb8f9a43a265d4e341b8072a74214f9e59e4b
nasirhussain <nasirhussain0@outlook.com>
2019-06-09 22:14:29 +0100

    api testing for VM

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\Controller;
 use App\User;
 use GuzzleHttp\Client;
 use DateTime;
-
+use Auth;
+use DB; 
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -23,12 +25,13 @@ class AuthController extends Controller
 
         $customerName = str_replace(' ', '',$request->fullname);
         $profilePic = $request->fullname;
-        $profilePicname = $profilePic->getClientOriginalName();
+        $profilePicname = 'test';
+               // $profilePicname = $profilePic->getClientOriginalName();
          // dd($profilePicname);
         $fileName = $customerName.$profilePicname;
         // dd($fileName);
         $uploadPicPath = 'profilePics/';
-        $profilePic->move($uploadPicPath,$fileName);
+        // $profilePic->move($uploadPicPath,$fileName);
         $profilePicUrl = $uploadPicPath.$fileName;
         // dd($profilePicUrl);
 
@@ -46,22 +49,90 @@ class AuthController extends Controller
         $user->payRate= 0;
         $user->numOfHolidays= 0;
         $user->save();
+        // dd($user);
+
+        $getUserId = DB::getPdo()->lastInsertId();
+        // dd($getUserId);
+        $getUserDetails = DB::table('users')->where('id', $getUserId)->first();
+        // dd($getUserDetails);
+
+        $email =  $getUserDetails->email;
+        $password = $getUserDetails->password;
+        // dd($email,  $password);
+
+        // 'csrf-token' => csrf_token()  
 
         $http = new Client;
+        // $response = User::all();
+        // dd($response);
+       // its from here the url
+        $response = $http->post(url('oauth/token'), [
+            'form_params' => [
+                'grant_type' => 'password',
+                'client_id' => '2',
+                'client_secret' => 'yetm6ZZ9g5aPeMC7VNqrxwVdP56MDG3mPuGY7Xgw',
+                'username' => $request->email,
+                'password' => $request->password,
+                'scope' => '',
+                         
+            ],
 
-	    $response = $http->post('http://127.0.0.1:8000/oauth/token', [
-	        'form_params' => [
-	            'grant_type' => 'password',
-	            'client_id' => '2',
-	            'client_secret' => 'TRsfBYRbuWdJAk4crUlPQYAP09aRKAt1701MwnR8',
-	            'username' => $request->email,
-	            'password' => $request->password,
-	            'redirect_uri' => 'http://example.com/callback',
-	            'code' => $request->code,
-	        ],
-	    ]);
-// dd(response(['data' => json_decode((string) $response->getBody(), true)]));
-	    return response(['data' => json_decode((string) $response->getBody(), true)]);
+        ]);
+
+        return response(['data'=>json_decode((string)$response->getBody(),true)]);
+
+
+
+
+       //  $http = new \GuzzleHttp\Client();
+       // // dd($http);
+       //  $request = $http->post(url('oauth/token'), [
+       //      'form_params' => [
+       //          'grant_type' => 'password',
+       //          'client_id' => 2,
+       //          'client_secret' => 'yetm6ZZ9g5aPeMC7VNqrxwVdP56MDG3mPuGY7Xgw',       
+       //      ],
+       //  ]);
+
+       //  return $request->getStatusCode(); 
+
+       //  // return $request->getBody();
+
+       //  // return response(['data'=>json_decode((string) $response->getBody(), true)]);
+
+
+
+
+
+
+//         $auth =  $response->getBody();
+//         dd($auth);
+// // return $response;
+//         return response(['data'=>json_decode((string) $response->getBody(), true)]);
+
+
+
+
+
+
+// return response(['data' => json_decode((string) $response->getBody(), true)]);
+        // return json_decode((string) $response->getBody(), true);
+
+//         $http = new Client;
+
+// 	    $response = $http->post('http://127.0.0.1:8000/oauth/token', [
+// 	        'form_params' => [
+// 	            'grant_type' => 'password',
+// 	            'client_id' => '2',
+// 	            'client_secret' => 'TRsfBYRbuWdJAk4crUlPQYAP09aRKAt1701MwnR8',
+// 	            'username' => $request->email,
+// 	            'password' => $request->password,
+// 	            'redirect_uri' => 'http://example.com/callback',
+// 	            'code' => $request->code,
+// 	        ],
+// 	    ]);
+// dd($response);
+// 	    return response(['data' => json_decode((string) $response->getBody(), true)]);
 
 
     }
--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/register','Api\AuthController@register');
+
 // Route::post('/login','Api\AuthController@login');
 // Route::apiResource('contact','Api\ContactController');
